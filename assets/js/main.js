aab8724f03b79dba4fef97cbc26b16f2fa512efd
nvbinh15 <binh.pdc01@gmail.com>
2021-06-09 21:43:52 +0800

    fix bugs, add skills and education

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,34 +28,39 @@ navLink.forEach(n => n.addEventListener('click', linkAction));
 /*===== SCROLL REVEAL ANIMATION =====*/
 const sr = ScrollReveal({
     origin: 'top',
-    distance: '80px',
-    duration: 1500,
+    distance: '50px',
+    duration: 1000,
     reset: true
 });
 
 /*SCROLL HOME*/
 sr.reveal('.home__title',{}); 
-sr.reveal('.button',{delay: 200}); 
-sr.reveal('.home__img',{delay: 400}); 
-sr.reveal('.home__social-icon',{ interval: 200}); 
+sr.reveal('.button',{delay: 100}); 
+sr.reveal('.home__img',{delay: 200}); 
+sr.reveal('.home__social-icon',{ interval: 100}); 
 
 /*SCROLL ABOUT*/
 sr.reveal('.about__img',{}); 
-sr.reveal('.about__subtitle',{delay: 400}); 
-sr.reveal('.about__text',{delay: 400}); 
+sr.reveal('.about__subtitle',{delay: 200}); 
+sr.reveal('.about__text',{delay: 200}); 
 
 /*SCROLL SKILLS*/
-sr.reveal('.skills__subtitle',{}); 
-sr.reveal('.skills__text',{}); 
-sr.reveal('.skills__data',{interval: 200}); 
-sr.reveal('.skills__img',{delay: 600});
+// sr.reveal('.skills__subtitle',{}); 
+// sr.reveal('.skills__text',{}); 
+// sr.reveal('.skills__data',{interval: 100}); 
+// sr.reveal('.skills__img',{delay: 300});
+sr.reveal('.skills', {interval: 100})
 
 /*SCROLL WORK*/
-sr.reveal('.work__img',{interval: 200}); 
+sr.reveal('.work__img',{interval: 100}); 
 
 /*SCROLL CONTACT*/
-sr.reveal('.contact__input',{interval: 200}); 
+sr.reveal('.contact__input',{interval: 100}); 
 
+/*SCROLL EDUCATION*/
+sr.reveal('.content',{interval: 100})
+sr.reveal('.tine',{interval: 100})
+sr.reveal('h4',{interval: 100})
 
 
 
